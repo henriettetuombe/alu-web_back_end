9322687e84a1e23e5a8d88e1f50fee5c35d672df
Henriette Tuombe <116841112+henriettetuombe@users.noreply.github.com>
2024-10-13 20:42:24 +0200

    Update 1-app.py

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python3
-"""
-0x0A. i18n
-"""
-from os import getenv
+''' Flask app '''
+
 from flask import Flask, render_template
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """class config"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    ''' App config '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """hello world"""
-    return render_template("1-index.html", message="Welcome to Holberton")
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run()
