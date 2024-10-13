844d92cd4ed01da887d30c5ba55261bfff1f532e
Henriette Tuombe <116841112+henriettetuombe@users.noreply.github.com>
2024-10-13 20:46:28 +0200

    Update 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""
-0x0A. i18n
-"""
-from os import getenv
-from flask import Flask, render_template, request
-from flask_babel import Babel
+''' Flask app '''
 
+from flask import Flask, request, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
-    """class config"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    ''' App config '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
@@ -23,17 +20,15 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """get the locale"""
+    ''' return best languages '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """hello world"""
-    return render_template("1-index.html", message="Welcome to Holberton")
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
+    return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run()
