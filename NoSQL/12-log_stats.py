6d7fb6b202a5f9ab17b4464aed777938156f1d3e
Henriette Tuombe <116841112+henriettetuombe@users.noreply.github.com>
2024-10-21 16:30:55 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""log stats from collection
-"""
+"""python scripts"""
 from pymongo import MongoClient
 
 
+
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-PIPE = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}}, {"$limit": 10}]
 
 
 def log_stats(mongo_collection, option=None):
@@ -26,10 +24,6 @@ def log_stats(mongo_collection, option=None):
         log_stats(nginx_collection, method)
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
-    print("IPs:")
-
-    for ip in mongo_collection.aggregate(PIPE):
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
 
 
 if __name__ == "__main__":
